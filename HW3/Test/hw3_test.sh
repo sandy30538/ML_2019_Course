c516e1ea9dce9934fef4a19897ad1396ad573eb9
sandy30538 <b03901160@ntu.edu.tw>
2020-09-24 14:29:27 +0800

    Add files via upload

---
+++ HW3/Test/hw3_test.sh
@@ -0,0 +1,7 @@
+wget https://github.com/sandy0716/ML_Model/releases/download/0.0.1/model_21.h5 & 
+wget https://github.com/sandy0716/ML_Model/releases/download/0.0.0/model_20.h5 &
+wget https://github.com/sandy0716/ML_Model/releases/download/0.0.3/model_18.h5 &
+wget https://github.com/sandy0716/ML_Model/releases/download/0.0.4/model_16.h5 &
+wget https://github.com/sandy0716/ML_Model/releases/download/0.0.5/model_13.h5 &
+wget https://github.com/sandy0716/ML_Model/releases/download/0.0.2/model_22.h5
+python3 hw3_test.py $1 $2
